3e12437113f36d35a9adbaf9788e9d98e71365da
dominic-source <chinonsodomnic@gmail.com>
2023-10-28 15:43:24 -0700

    i want to branch

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,31 @@
+#!/usr/bin/python3
+
+"""This module contains the route for our api"""
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
+
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """ returns status in json """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('stats', methods=['GET'])
+def stats():
+    """Return the stats in dict format"""
+    new_dict = {}
+    for key, value in classes.items():
+        count = storage.count(value)
+        new_dict[key] = count
+    return jsonify(new_dict)
