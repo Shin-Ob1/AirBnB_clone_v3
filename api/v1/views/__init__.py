98f337c714e296b140143ce814147711015c46c6
dominic-source <chinonsodomnic@gmail.com>
2023-10-29 08:12:26 -0700

    update views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,3 +6,14 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
+# Import state views here
+
+# Import user views here
+
+# Import amenity views here
+
+# Import city views here
+
+# Import place views here
+
+# Import review views here
