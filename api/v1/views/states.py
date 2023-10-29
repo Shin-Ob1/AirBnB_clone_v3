be581f886a2e616b7872788ba8e9979c66bcc78a
dominic-source <chinonsodomnic@gmail.com>
2023-10-29 15:41:50 -0700

    debug state file v5

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -39,6 +39,7 @@ def get_all_state(state_id=None):
             abort(404)
         else:
             storage.delete(data)
+            storage.save()
             return jsonify({}), 200
     elif request.method == 'POST' and state_id is None:
         data = request.get_json()
