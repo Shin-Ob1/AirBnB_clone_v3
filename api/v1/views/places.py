2b945ccd3df0a689e65f90887bbf56abec0421f7
dominic-source <chinonsodomnic@gmail.com>
2023-10-30 06:32:23 -0700

    debug place v2

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -76,7 +76,8 @@ def get_places(place_id=None):
             if js is None:
                 return 'Not a JSON', 400
             for key, value in js.items():
-                if key not in ['id', 'created_at', 'updated_at', 'user_id', 'city_id']:
+                if key not in ['id', 'created_at',
+                               'updated_at', 'user_id', 'city_id']:
                     setattr(data, key, value)
             storage.save()
             return jsonify(data.to_dict()), 200
