904b203d96c1c4a5b52a857c68fabb7f3ceebbec
ShinOb1 <obikelv@gmail.com>
2023-10-30 12:23:07 -0700

    deletes a review

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -40,6 +40,6 @@ def delete_review(review_id):
     review = storage.get(Review, review_id)
     if review is None:
         abort(404)
-    storage.delete(review)
+    review.delete()
     storage.save()
     return jsonify({}), 200
