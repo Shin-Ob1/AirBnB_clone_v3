28dfda0da5bddb9d90203c30e2a7b1fa51206ce1
ShinOb1 <obikelv@gmail.com>
2023-10-30 11:58:49 -0700

    get a review from reviews

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -21,3 +21,13 @@ def get_reviews(place_id):
     for pl in place.reviews:
         rev_list.append(pl.to_dict())
     return jsonify(rev_list)
+
+
+@app_views.route('/reviews/<string:review_id>', methods=['GET'],
+                 strict_slashes=False)
+def get review(review_id):
+    """ Retrieves a review """
+    review = storage.get(Review, review_id)
+    if review is None:
+        abort(404)
+    return jsonify(review.to_dict())
